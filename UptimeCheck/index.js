cb56da8a86bbee4b45d229c2353e61588624b3f0
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-08-23 18:38:57 -0400

    updating server for accepting the required fields from browser request

--- UptimeCheck/index.js
+++ UptimeCheck/index.js
@@ -1,7 +1,39 @@
 var http = require('http');
+var url = require('url');
+var stringDecoder = require('string_decoder').StringDecoder;
 
-var server = http.createServer(function (req, res) {
-    res.end();
+http.createServer(function (req, res) {
+
+    var path = url.parse(req.url).pathname;
+    var query = url.parse(req.url,true).query;
+    var method = req.method;
+    var headers = req.headers;
+    
+    var dataStream;
+    var decoder = new stringDecoder('utf-8');
+
+    req.on('data',function(data){
+        dataStream += decoder.write(data);
+    });
+    req.on('end',function(){
+        
+        dataStream += decoder.end();
+
+        var inData = {
+            "path":path,
+            "query":query,
+            "method":method,
+            "headers":headers,
+            "payload": dataStream
+        };
+
+        console.log(inData);
+
+        res.setHeader('Content-Type',"application/json");
+        res.writeHead(200);
+        res.end(JSON.stringify({"SUCCESS":"SERVER WORKING"}));
+    });
+
 }).listen(2500, function () {
     console.log("\x1b[47m\x1b[31m",
         "HTTP server started @ port : 2500",
