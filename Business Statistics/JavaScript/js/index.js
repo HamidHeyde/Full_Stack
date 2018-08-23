f223da1b2ff14fca842d2be9e51b6cedb2ed992c
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-08-23 15:40:55 -0400

    Starting A New Repository

---
+++ Business Statistics/JavaScript/js/index.js
@@ -0,0 +1,116 @@
+//OptionIcons
+var OpttionIcons = document.getElementsByClassName('optionsIcon');
+for ( i=1; i<OpttionIcons.length; i++)
+{
+  OpttionIcons[i].addEventListener("click", function(){
+  OptionMenuOpen(this);});
+}
+
+//CloseButtons
+var CloseIcons = document.getElementsByClassName('closeIcon');
+for ( i=0; i<CloseIcons.length; i++)
+{
+  CloseIcons[i].addEventListener("click", function(){
+  OptionDivClose(this);});
+}
+
+//subMenus
+var subMenus = document.getElementsByClassName('subMenu');
+for ( i=0; i<subMenus.length; i++)
+{
+  if ((i+2)%2 == 0)
+  {
+    subMenus[i].addEventListener("click", function(){
+    OptionMenuClose(this);});
+  }
+  else
+  {
+    subMenus[i].addEventListener("click", function(){
+    OptionDivOpen(this);});
+  }
+}
+
+//floating Divs
+var chartDivs = document.getElementsByClassName('chartWrapper');
+for ( i=0; i<chartDivs.length; i++)
+{
+  chartDivs[i].addEventListener("mousedown", function(){
+  dragElement(this,event);});
+}
+
+function dragElement(elmnt,e) {
+
+  var cx = parseInt(elmnt.style.left)?parseInt(elmnt.style.left):0;
+  var cy = parseInt(elmnt.style.top)?parseInt(elmnt.style.top):0;
+
+  var pos1 = e.clientX;
+  var pos2 = e.clientY;
+
+  document.onmouseup = closeDragElement;
+  document.onmousemove = elementDrag;
+
+  function elementDrag(e) {
+    var pos3 = e.clientX;
+    var pos4 = e.clientY;
+
+    var l = (pos3-pos1);
+    var t = (pos4-pos2);
+
+    elmnt.style.left = (cx+l) + "px";
+    elmnt.style.top =  (cy+t) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+function AccessMenu(div)
+{
+  var subwrapper = div.childNodes[5];
+  subwrapper.classList.toggle("clicked");
+}
+function SetAccess(div,a,b,c,d)
+{
+  var title = document.getElementsByClassName('selected');
+  title[0].innerHTML = div.innerHTML;
+
+  var charts = document.getElementsByClassName('chartWrapper');
+  if (a==0){a='none';} else {a='block';}
+  if (b==0){b='none';} else {b='block';}
+  if (c==0){c='none';} else {c='block';}
+  if (d==0){d='none';} else {d='block';}
+  charts[0].style.display = a;
+  charts[1].style.display = b;
+  charts[2].style.display = c;
+  charts[3].style.display = d;
+  placeCharts();
+}
+function OptionMenuOpen (ClickedDiv)
+{
+  var icon = ClickedDiv.parentNode.parentNode.childNodes[1].childNodes[5];
+  var menu = icon.childNodes[3];
+  icon.classList.toggle("clicked");
+  menu.classList.toggle("clicked");
+}
+function OptionMenuClose(ClickedDiv)
+{
+    var ChartDiv = ClickedDiv.parentNode.parentNode.parentNode.parentNode.childNodes[3];
+    ChartDiv.classList.toggle("clicked");
+
+    if (ClickedDiv.innerHTML == "Close")
+    {ClickedDiv.innerHTML = "Open"}
+    else {ClickedDiv.innerHTML = "Close"}
+  }
+  function OptionDivOpen(ClickedDiv)
+  {
+    var ChartDiv = ClickedDiv.parentNode.parentNode.parentNode.parentNode.childNodes[3];
+    var OptionsDiv  = ChartDiv.childNodes[1];
+    OptionsDiv.style.display = 'block';
+  }
+  function OptionDivClose(ClickedDiv)
+  {
+    var OptionsDiv = ClickedDiv.parentNode.parentNode
+    OptionsDiv.style.display = 'none';
+  }
