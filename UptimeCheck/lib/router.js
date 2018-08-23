184149b560a13ae2f2e71865cf5715362eae90c6
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-08-23 19:53:17 -0400

    adding router

---
+++ UptimeCheck/lib/router.js
@@ -0,0 +1,12 @@
+var apiHandlers = require('./apiHandlers');
+
+var router = {
+    //basics
+    "notFound":apiHandlers.notFound,
+    "/ping":apiHandlers.ping
+    //api
+    //html
+};
+
+module.exports = router;
+
