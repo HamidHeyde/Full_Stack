570889f5822460213129746a9779a2ee4c00765c
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-08-23 20:02:37 -0400

    handling incomming request for ping/notfound

--- UptimeCheck/index.js
+++ UptimeCheck/index.js
@@ -11,37 +11,51 @@ var debug = util.debuglog('index');
 http.createServer(function (req, res) {
 
     var path = url.parse(req.url).pathname;
-    var query = url.parse(req.url,true).query;
+    var query = url.parse(req.url, true).query;
     var method = req.method;
     var headers = req.headers;
-    
+
     var dataStream;
     var decoder = new stringDecoder('utf-8');
 
-    req.on('data',function(data){
+    req.on('data', function (data) {
         dataStream += decoder.write(data);
     });
-    req.on('end',function(){
-        
+    req.on('end', function () {
+
         dataStream += decoder.end();
 
         var inData = {
-            "path":path,
-            "query":query,
-            "method":method,
-            "headers":headers,
+            "path": path,
+            "query": query,
+            "method": method,
+            "headers": headers,
             "payload": dataStream
         };
 
+        var handler = (typeof (router[path]) != 'undefined') ? router[path] : router['notFound'];
+        handler(inData, function (statusCode, cType, data) {
 
-        res.setHeader('Content-Type',"application/json");
-        res.writeHead(200);
-        res.end(JSON.stringify({"SUCCESS":"SERVER WORKING"}));
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+            cType = typeof (cType) == 'string' ? cType : 'json';
+
+            if (cType == 'json') {
+                cType = "application/json";
+                data = typeof (data) == 'object' ? data : {};
+                data = JSON.stringify(data);
+            }
+
+
+            res.setHeader('Content-Type', cType);
+            res.writeHead(statusCode);
+            res.end(data);
+
+        });
     });
 
 }).listen(config.env.port, function () {
     console.log("\x1b[47m\x1b[31m",
-        "HTTP server started @ port : ",config.env.port,
-        "\n[env:", config.env.name,"]",
+        "HTTP server started @ port : ", config.env.port,
+        "\n[env:", config.env.name, "]",
         "\x1b[0m");
 });
