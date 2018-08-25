96c960a02712801de8e5d5055494fccc82baace3
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-08-25 16:54:38 -0400

    CORS problem

--- UptimeCheck/index.js
+++ UptimeCheck/index.js
@@ -45,6 +45,10 @@ http.createServer(function (req, res) {
                 data = JSON.stringify(data);
             }
 
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Credentials', true);
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
             res.setHeader('Content-Type', cType);
             res.writeHead(statusCode);
