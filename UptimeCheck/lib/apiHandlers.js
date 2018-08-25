5f59378a906bebda101262673f3ed79c7090ff45
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-08-25 16:54:51 -0400

    absic routes functions added

--- UptimeCheck/lib/apiHandlers.js
+++ UptimeCheck/lib/apiHandlers.js
@@ -10,4 +10,48 @@ handlers.ping = function (data, callback) {
         { "success": "site is up!" });
 };
 
+//General User Methods
+handlers.users = function(data,callback){
+    var allowedMethods = ['get','put','delete','post','options'];
+    var method = (allowedMethods.indexOf(data.method)>-1)? data.method :false;
+    if (method){
+        handlers._users[method](data,callback);
+    }else{
+        callback(405, 'json',
+        { "error": "required method, not allowed my friend!" });
+    }
+};
+
+
+handlers._users = {};
+//Users Get
+handlers._users.get = function(data,callback){
+    console.log(data.method,"\n",data.query);
+    callback(200, 'json',
+        { "success": "users.get" });
+};
+//Users POST
+handlers._users.post = function(data,callback){
+    console.log(data.method,"\n",data.query);
+    callback(200, 'json',
+        { "success": "users.post" });
+};
+//Users PUT
+handlers._users.put = function(data,callback){
+    console.log(data.method,"\n",data.query);
+    callback(200, 'json',
+        { "success": "users.put" });
+};
+//Users DELETE
+handlers._users.delete = function(data,callback){
+    console.log(data.method,"\n",data.query);
+    callback(200, 'json',
+        { "success": "users.delete" });
+};
+//Users OPTIONS
+handlers._users.options = function(data,callback){
+    console.log(data.method,"\n",data.query);
+    callback(200, 'json',
+        { "success": "users.options" });
+};
 module.exports = handlers;
