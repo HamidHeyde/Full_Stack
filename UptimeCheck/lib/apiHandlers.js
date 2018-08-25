1f89734887fbb100a0128e15a9ceec5df14c4185
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-08-25 18:29:15 -0400

    users db config, Added

--- UptimeCheck/lib/apiHandlers.js
+++ UptimeCheck/lib/apiHandlers.js
@@ -1,3 +1,6 @@
+var config = require('./config');
+
+
 var handlers = {};
 
 handlers.notFound = function (data, callback) {
@@ -11,46 +14,50 @@ handlers.ping = function (data, callback) {
 };
 
 //General User Methods
-handlers.users = function(data,callback){
-    var allowedMethods = ['get','put','delete','post','options'];
-    var method = (allowedMethods.indexOf(data.method)>-1)? data.method :false;
-    if (method){
-        handlers._users[method](data,callback);
-    }else{
+handlers.users = function (data, callback) {
+    var allowedMethods = ['get', 'put', 'delete', 'post', 'options'];
+    if (allowedMethods.indexOf(data.method) > -1) {
+        //db config for Users
+        data.db = {
+            url: config.db.url,
+            db: "uptimeCheck",
+            collection: "users"
+        };
+        handlers._users[data.method](data, callback);
+    } else {
         callback(405, 'json',
-        { "error": "required method, not allowed my friend!" });
+            { "error": "required method, not allowed my friend!" });
     }
 };
 
-
 handlers._users = {};
 //Users Get
-handlers._users.get = function(data,callback){
-    console.log(data.method,"\n",data.query);
+handlers._users.get = function (data, callback) {
+    console.log(data.method, "\n", data.query,"\n",data.db);
     callback(200, 'json',
         { "success": "users.get" });
 };
 //Users POST
-handlers._users.post = function(data,callback){
-    console.log(data.method,"\n",data.query);
+handlers._users.post = function (data, callback) {
+    console.log(data.method, "\n", data.query);
     callback(200, 'json',
         { "success": "users.post" });
 };
 //Users PUT
-handlers._users.put = function(data,callback){
-    console.log(data.method,"\n",data.query);
+handlers._users.put = function (data, callback) {
+    console.log(data.method, "\n", data.query);
     callback(200, 'json',
         { "success": "users.put" });
 };
 //Users DELETE
-handlers._users.delete = function(data,callback){
-    console.log(data.method,"\n",data.query);
+handlers._users.delete = function (data, callback) {
+    console.log(data.method, "\n", data.query);
     callback(200, 'json',
         { "success": "users.delete" });
 };
 //Users OPTIONS
-handlers._users.options = function(data,callback){
-    console.log(data.method,"\n",data.query);
+handlers._users.options = function (data, callback) {
+    console.log(data.method, "\n", data.query);
     callback(200, 'json',
         { "success": "users.options" });
 };
