6b7d971f9e8cf097c13e700899b464751a2724f2
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-08-26 19:00:37 -0400

    update, Added

--- UptimeCheck/lib/db.js
+++ UptimeCheck/lib/db.js
@@ -14,28 +14,15 @@ db.connect = function (dbMethod, dbConfig, callback) {
             const collection = dbConfig.collection;
             const query = dbConfig.query;
 
-            if (dbMethod == "find") {
-                //Running Query
-                db.findUsers(dbName, collection, query, function (err, users) {
-                    (!err && users)
-                        ? callback(200, 'json', users)
-                        : callback(500, 'json', err);
-                });
-            }else if(dbMethod == "insert"){
-                db.insertUser(dbName, collection, query, function (err,message) {
-                    (!err)
-                        ? callback(200, 'json', message)
-                        : callback(500, 'json', err);
-                });
-            }else if(dbMethod == "update"){
+            //ASSIGNING EACH OPERATIONS TO ITS OWN METHOD
+            var functionality = (dbMethod+"User");
+            functionality = functionality.toString()
 
-            }else if(dbMethod == "delete"){
-                db.deleteUser(dbName, collection, query, function (err,message) {
-                    (!err)
-                        ? callback(200, 'json', message)
-                        : callback(500, 'json', err);
-                });
-            }
+            db[functionality](dbName, collection, query, function (err, message) {
+                (!err && message)
+                    ? callback(200, 'json', message)
+                    : callback(500, 'json', err);
+            });
 
             //CLOSING THE CONNECTION
             client.close();
@@ -45,7 +32,7 @@ db.connect = function (dbMethod, dbConfig, callback) {
     });
 };
 
-db.findUsers = function (database, dbCollection, query, callback) {
+db.findUser = function (database, dbCollection, query, callback) {
 
     database.collection(dbCollection).find(query).toArray(function (err, users) {
         if (!err && users) {
@@ -58,9 +45,20 @@ db.findUsers = function (database, dbCollection, query, callback) {
 
 db.insertUser = function (database, dbCollection, query, callback) {
 
-    database.collection(dbCollection).insertOne(query,function (err, result) {
-        if ((!err) && (result.insertedCount==1)) {
-            callback(false, {"SUCCESS":"User Submitted Successfuly"});
+    database.collection(dbCollection).insertOne(query, function (err, result) {
+        if ((!err) && (result.insertedCount == 1)) {
+            callback(false, { "SUCCESS": "User Submitted Successfuly" });
+        } else {
+            callback(true, { "ERROR": "Requested Query could not be executed" });
+        }
+    });
+};
+
+db.updateUser = function (database, dbCollection, query, callback) {
+
+    database.collection(dbCollection).updateOne(query.query,{$set:query.param} , function (err, result) {
+        if ((!err) && (result.matchedCount == 1)&& (result.modifiedCount == 1)) {
+            callback(false, { "SUCCESS": "User Deleted Successfuly" });
         } else {
             callback(true, { "ERROR": "Requested Query could not be executed" });
         }
@@ -69,9 +67,9 @@ db.insertUser = function (database, dbCollection, query, callback) {
 
 db.deleteUser = function (database, dbCollection, query, callback) {
 
-    database.collection(dbCollection).deleteOne(query,function (err, result) {
-        if ((!err) && (result.deletedCount==1)) {
-            callback(false, {"SUCCESS":"User Deleted Successfuly"});
+    database.collection(dbCollection).deleteOne(query, function (err, result) {
+        if ((!err) && (result.deletedCount == 1)) {
+            callback(false, { "SUCCESS": "User Deleted Successfuly" });
         } else {
             callback(true, { "ERROR": "Requested Query could not be executed" });
         }
