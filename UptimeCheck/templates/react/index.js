c555945d1244d227bf8038bb26128a08dcca540b
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-09-03 13:06:21 -0400

    moving components to its own file

--- UptimeCheck/templates/react/index.js
+++ UptimeCheck/templates/react/index.js
@@ -1,54 +1,87 @@
 //===========FUNCTIONS============
+var test = function(id, ev) {
+  console.log(id);
+  console.log(ev.target);
+};
+var topMenuClick = function(page, ev) {
+  state.currentComp = page;
+  //console.log(document.getElementsByTagName('link'));
+  renderApp();
+};
 //===========GLOBALS============
 var ce = React.createElement;
 var state = {
   currentComp: "index",
+  compMenuClick: topMenuClick,
   comps: {
     index: {
-      menus: ["Signin", "Signup"]
+      menus: {
+        items: ["Signin", "Signup"],
+        links: ["signin", "signup"]
+      }
     },
     signin: {
-      menus: ["Home", "Signup"]
+      menus: {
+        items: ["Home", "Signup"],
+        links: ["index", "signup"]
+      }
     },
     signup: {
-        menus: ["Home", "Signin"]
+      menus: {
+        items: ["Home", "Signin"],
+        links: ["index", "signin"]
+      }
     }
   }
 };
 //===========COMPONENTS===========
 //===========Header
-var Header = function(props){
-    var out = 
-    ce('div',{className:"header"},
-        ce('div',{className:"logo"},
-            ce('img',{src:"./images/logo.png",alt:"logo"})
-        ),
-        ce('div',{className:"menu"},
-            props.menus.map(function(element,index){
-                return(
-                    ce('div',{className:"item"},
-                        ce('a',{
-                            href:"./"+element.toLowerCase()+".html",
-                            className:"textLink"
-                            },
-                            element
-                        )
-                    )
-                )
-            })
-        )
-    );
+var Header = function(props) {
+  var out = ce(
+    "div",
+    { className: "header" },
+    ce(
+      "div",
+      { className: "logo" },
+      ce("img", { src: "./images/logo.png", alt: "logo" })
+    ),
+    ce(
+      "div",
+      { className: "menu" },
+      props.menus.items.map(function(element, index) {
+        return ce(
+          "div",
+          { className: "item" },
+          ce(
+            "a",
+            {
+              className: "textLink",
+              onClick: function(ev) {
+                props.menuClickAction(props.menus.links[index], ev);
+              }
+            },
+            element
+          )
+        );
+      })
+    )
+  );
 
-    return out;
+  return out;
 };
 //===========Main Application
 var App = function(props) {
-  var out = ce("div", { className: props.cName },
+  var out = ce(
+    "div",
+    { className: props.cName },
     //Rendering the Required Componens
     //Rendering Header
-    ce(Header, { menus: state.comps[state.currentComp].menus}),
+    ce(Header, {
+      menus: state.comps[state.currentComp].menus,
+      menuClickAction: state.compMenuClick
+    }),
     //Rendering Application
-    //ce(Application, { data: state.comps[state.currentComp] })
+    ce(Application, { data: state.comps[state.currentComp] })
   );
   return out;
 };
