1afc69de5c379c8819c273d13e8d55b64947ec7d
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-09-03 13:38:50 -0400

    Signin Component

---
+++ UptimeCheck/templates/react/signin.js
@@ -0,0 +1,31 @@
+//===========COMPONENTS===========
+var Application = function(props) {
+  var out = 
+  ce("div",{ className: "application" },
+    ce("form", {id:"signin", action:"/api/users",method:"GET"},
+      ce("div",{ className: "loginForm" },
+        ce("div",{ className: "header" },
+          ce("div", { className: "title" }, "LOGIN")
+        ),
+        ce("div", { className: "content" },
+            ce("div", { className: "row" },
+                ce("div", { className: "label" },"Username"),
+                ce("div", { className: "userInput" },
+                    ce("input", {name:"username",type:"text",placeholder:"Username"}),
+                ),
+            ),
+            ce("div", { className: "row" },
+                ce("div", { className: "label" },"Password"),
+                ce("div", { className: "userInput" },
+                    ce("input", {name:"password",type:"text",placeholder:"Password"}),
+                ),
+            ),
+            ce("div", { className: "row" },
+                ce("input", {type:"button",value:"Login"})
+            )
+        )
+      )
+    )
+  );
+  return out;
+};
