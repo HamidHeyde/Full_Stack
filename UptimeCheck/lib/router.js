120fe24c121ab2fa01df9bae7890a7c7cf0ccac0
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-09-04 18:53:43 -0400

    adding HTML index route

--- UptimeCheck/lib/router.js
+++ UptimeCheck/lib/router.js
@@ -1,12 +1,14 @@
 var apiHandlers = require('./apiHandlers');
+var htmlHandlers = require('./htmlHandlers');
 
 var router = {
     //basics
     "notFound":apiHandlers.notFound,
     "/ping":apiHandlers.ping,
     //api
-    "/api/users":apiHandlers.users
+    "/api/users":apiHandlers.users,
     //html
+    "/":htmlHandlers.index
 };
 
 module.exports = router;
