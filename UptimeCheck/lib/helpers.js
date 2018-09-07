f73aafde0d6f346e3b33816fcc6bc88bc4924b4b
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-09-07 14:44:12 -0400

    codes for page variable interpolation

--- UptimeCheck/lib/helpers.js
+++ UptimeCheck/lib/helpers.js
@@ -1,7 +1,31 @@
 var helpers = {};
 
-helpers.placePageVariables = function(page,pageVariables,callback){
-    
+helpers.placePageVariables = function (page, pageVariables,callback) {
+    //Page Variables to String
+    pageVar = {};
+    for (key in pageVariables) {
+        if (pageVariables.hasOwnProperty(key)) {
+            // console.log(key);
+            for (subKey in pageVariables[key]) {
+                // console.log(subKey);
+                if (pageVariables[key].hasOwnProperty(subKey)) {
+                    keyToEnter = key.toString() + "." + subKey.toString();
+                    pageVar[keyToEnter] = pageVariables[key][subKey];
+                }
+            }
+        }
+    };
+    // console.log(pageVariables);
+    // console.log(pageVar);
+    for (key in pageVar) {
+        if (pageVar.hasOwnProperty(key)) {
+            var find = '{' + key + '}';
+            var replace = pageVar[key];
+              page = page.replace(find, replace);
+        }
+    };
+
+    callback(page);
 };
 
 module.exports = helpers;
