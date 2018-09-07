4ef25ab54660e95d1b0093680f31534e0c10d80f
[Hamid] <heidarzadeh.hamidreza@gmail.com>
2018-09-07 14:43:46 -0400

    codes for assebling a HTML page

--- UptimeCheck/lib/fileOps.js
+++ UptimeCheck/lib/fileOps.js
@@ -1,47 +1,76 @@
-var fs = require('fs');
-var path = require('path');
+var fs = require("fs");
+var path = require("path");
 
 var fileOps = {};
 
-fileOps.finalPath = function(dir,file){
-    var currentDir = __dirname;
-    var fPath;
-    if(dir == 'templates'){
-        //For Template Folder [REACT]
-        fPath  = path.join(currentDir, '/../templates/');
-        fPath+=file;
-    }else if(dir == 'public'){
-        //For Template Folder [REACT]
-        fPath  = path.join(currentDir, '/..');
-        fPath+=file;
-    }
-    return fPath;
+fileOps.finalPath = function (dir, file) {
+  var currentDir = __dirname;
+  var fPath;
+  if (dir == "public") {
+    //For Template Folder [REACT]
+    fPath = path.join(currentDir, "/..");
+    fPath += file;
+  } else {
+    //For Template Folder [REACT]
+    fPath = path.join(currentDir, "/../");
+    fPath += dir;
+    fPath += file;
+  }
+  return fPath;
 };
 //Read file
-fileOps.read = function (dir,file,callback){
-    
-    var filePath = fileOps.finalPath(dir,file);
+fileOps.read = function (dir, file, callback) {
+  var filePath = fileOps.finalPath(dir, file);
 
-    fs.readFile(filePath, `utf-8`, function(err, data) {
-        if (!err && data) {
-            callback(false, data);
-        } else {
-            callback(true,err);
-        }
-    });
+  fs.readFile(filePath, `utf-8`, function (err, data) {
+    if (!err && data) {
+      callback(false, data);
+    } else {
+      callback(true, err);
+    }
+  });
 };
 
-fileOps.readStatics = function (dir,file,callback){
-    
-    var filePath = fileOps.finalPath(dir,file);
-    //callback(filePath);
-    fs.readFile(filePath, function(err, data) {
-        if (!err && data) {
-            callback(false, data);
+//Read Static Files
+fileOps.readStatics = function (dir, file, callback) {
+  var filePath = fileOps.finalPath(dir, file);
+  //callback(filePath);
+  fs.readFile(filePath, function (err, data) {
+    if (!err && data) {
+      callback(false, data);
+    } else {
+      callback(true, err);
+    }
+  });
+};
+
+//Assemble Html Pages (Header + Body + Footer)
+fileOps.assembleHtmlPage = function (dir, file, callback) {
+  
+  var headerPath = fileOps.finalPath(dir, "header.html");
+  var bodyPath = fileOps.finalPath(dir, file);
+  var footerPath = fileOps.finalPath(dir, "footer.html");
+
+  fs.readFile(headerPath, function (err, header) {
+    if (!err && header) {
+      fs.readFile(bodyPath, function (err, body) {
+        if (!err && body) {
+          fs.readFile(footerPath, function (err, footer) {
+            if (!err && footer) {
+              var finalPage = header + body + footer;
+              callback(false, finalPage);
+            } else {
+              callback(true, err);
+            }
+          });
         } else {
-            callback(true,err);
+          callback(true, err);
         }
-    });
+      });
+    } else {
+      callback(true, err);
+    }
+  });
 };
 
 module.exports = fileOps;
